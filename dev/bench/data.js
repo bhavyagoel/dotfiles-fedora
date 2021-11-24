45c5525f6e204001bb0273bbe7d5c7b099ac9832
github-action-benchmark <github@users.noreply.github.com>
2021-11-24 03:42:24 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7e2e4f6da741f7d489ec4c411b20b03b74f93edd

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637725344100,
+  "repoUrl": "https://github.com/yutkat/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "8683947+yutkat@users.noreply.github.com",
+            "name": "Yuta Katayama",
+            "username": "yutkat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7e2e4f6da741f7d489ec4c411b20b03b74f93edd",
+          "message": "Merge pull request #2483 from yutkat/v39.002",
+          "timestamp": "2021-11-24T12:37:40+09:00",
+          "tree_id": "371d0a20d900e73fba6293642f80c5b1b7ae0465",
+          "url": "https://github.com/yutkat/dotfiles/commit/7e2e4f6da741f7d489ec4c411b20b03b74f93edd"
+        },
+        "date": 1637725343248,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 0.016,
+            "unit": "Second"
+          }
+        ]
+      }
+    ]
+  }
+}
