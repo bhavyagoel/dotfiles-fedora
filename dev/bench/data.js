b768d2f7404ee44304ff2ab6bc4fb84d93974c57
github-action-benchmark <github@users.noreply.github.com>
2022-05-20 16:12:39 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b6b69093da86c9f8bc1d0f67d3b8a084f35e96b3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653055006351,
+  "lastUpdate": 1653063159599,
   "repoUrl": "https://github.com/yutkat/dotfiles",
   "entries": {
     "Benchmark": [
@@ -10571,6 +10571,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "neovim load time",
             "value": 0.073,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "8683947+yutkat@users.noreply.github.com",
+            "name": "Yuta Katayama",
+            "username": "yutkat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b6b69093da86c9f8bc1d0f67d3b8a084f35e96b3",
+          "message": "Merge pull request #2873 from yutkat/v42.232\n\nneovim: Disable ziontee113/syntax-tree-surfer because I use tree-sitter incremental selection",
+          "timestamp": "2022-05-21T01:09:39+09:00",
+          "tree_id": "51abc3256e407efb709cf8c720d78201704da28b",
+          "url": "https://github.com/yutkat/dotfiles/commit/b6b69093da86c9f8bc1d0f67d3b8a084f35e96b3"
+        },
+        "date": 1653063159295,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 0.21,
+            "unit": "Second"
+          },
+          {
+            "name": "neovim load time",
+            "value": 0.069,
             "unit": "Second"
           }
         ]
